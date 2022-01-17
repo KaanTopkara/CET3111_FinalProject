778497d45d5c234ac087080802ec16d89eafd17a
Furkan Kaan Topkara <69640591+KaanTopkara@users.noreply.github.com>
2022-01-17 07:33:01 +0300

    last commites

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static bool movementActive = true;
-    private bool levelPassed = false;
+    public static bool levelPass = false;
+    public static bool levelPassed = false;
     [SerializeField] GameObject DialogueWindow;
+    private GameObject Talk;
     [SerializeField] private float dialogueTime = 1f;
     void Start()
     {
         DialogueWindow.SetActive(false);
+        Talk = DialogueWindow.gameObject.transform.GetChild(0).gameObject;
         if (SceneManager.GetActiveScene().name == "Game1")
         {
             movementActive = false;
@@ -20,31 +24,36 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (levelPass)
+        {
+            nextLevel();
+        }
+    }
+
     public void nextLevel()
     {
-        Debug.Log("Iamcalled");
         if (movementActive && levelPassed)
         {
-            Debug.Log("ImcalledToo");
             levelPassed = false;
             SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1));
+            levelPass = false;
         }
     }
 
     IEnumerator startDialogue()
     {
         DialogueWindow.SetActive(true);
-        DialogueWindow.transform.GetChild(0).gameObject.SetActive(true);
+        Talk.GetComponent<TextMeshPro>().text = "Buraya nasıl geldim?";
         yield return new WaitForSeconds(dialogueTime);
-        DialogueWindow.transform.GetChild(0).gameObject.SetActive(false);
-        DialogueWindow.transform.GetChild(1).gameObject.SetActive(true);
+        Talk.GetComponent<TextMeshPro>().text = "Nerede oldugumu ve ne yapacagimi bilmiyorum?";
         yield return new WaitForSeconds(dialogueTime);
-        DialogueWindow.transform.GetChild(1).gameObject.SetActive(false);
-        DialogueWindow.transform.GetChild(2).gameObject.SetActive(true);
+        Talk.GetComponent<TextMeshPro>().text = "Sanirim ileri gitmeliyim. Tek yol bu gibi gözüküyor.";
         yield return new WaitForSeconds(dialogueTime);
-        DialogueWindow.transform.GetChild(2).gameObject.SetActive(false);
         DialogueWindow.SetActive(false);
         movementActive = true;
         levelPassed = true;
     }
+    
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -10,17 +11,24 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D playerRB;
     private float lastPosition = 0;
     [SerializeField] private GameObject interActionGuide;
+    private GameObject TextBubble;
+    [SerializeField] private GameObject infoDocPanel;
     private GameManager managerGame;
+    private int levelCondition = 0;
+    public GameObject interactiobObjectHolder;
     void Start()
     {
         animatorPlayer = GetComponent<Animator>();
         playerRB = gameObject.GetComponent<Rigidbody2D>();
+        TextBubble = gameObject.transform.GetChild(0).gameObject;
+        levelCondition = 0;
     }
 
     void Update()
     {
         float dirX = Input.GetAxis ("Horizontal") ;
         lastPosition = transform.position.x;
+        Debug.Log(levelCondition);
 
         if (transform.position.x >= 40 && transform.position.x <=1970 && GameManager.movementActive)
         {
@@ -47,35 +55,155 @@ public class PlayerController : MonoBehaviour
             }
             playerRB.velocity= new Vector2(dirX * movementSpeed, 0);
         }
+        else
+        {
+            playerRB.velocity = Vector2.zero;
+        }
 
         if (PropTrigger.textBubble)
         {
             transform.GetChild(0).gameObject.SetActive(true);
         }
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("collisionHappend");
-        if (collision.gameObject.name == "RightSide" )
+
+        if (infoDocPanel.activeSelf && Input.GetKeyDown(KeyCode.R))
         {
-           managerGame.nextLevel();
+            for (int i = 0; i < 5; i++)
+            {
+                infoDocPanel.transform.GetChild(i).gameObject.SetActive(false);
+            }
+            infoDocPanel.SetActive(false);
+            GameManager.movementActive = true;
+
+        }
+        if (levelCondition >= 2)
+        {
+            GameManager.levelPassed = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "RightSide")
+        {
+            GameManager.levelPass = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        TextBubble.SetActive(false);
+        if (other.gameObject.name == "Cable")
+        {
+            interActionGuide.SetActive(true);
+            TextBubble.gameObject.SetActive(true);
+            TextBubble.transform.GetChild(0).GetComponent<TextMeshPro>().text = "Bu sey de nedir boyle?";
+        }
+        if (other.gameObject.name == "Battery")
+        {
+            interActionGuide.SetActive(true);
+        }
+        if (other.gameObject.name == "Lamp")
+        {
+            interActionGuide.SetActive(true);
+            TextBubble.gameObject.SetActive(true);
+            TextBubble.transform.GetChild(0).GetComponent<TextMeshPro>().text = "Bu sey de nedir boyle?";
         }
 
-        if (collision.gameObject.name == "Cable")
+        if (other.gameObject.name == "Switch")
         {
-            
+            interActionGuide.SetActive(true);
         }
-        if (collision.gameObject.name == "Battery")
+
+        if (other.gameObject.name == "Doc")
         {
-            
+            interActionGuide.SetActive(true);
+            TextBubble.gameObject.SetActive(true);
+            TextBubble.transform.GetChild(0).GetComponent<TextMeshPro>().text = "Bu kağıtlar da ne böyle?";
         }
-        if (collision.gameObject.name == "Lamp")
+    }
+
+    private void OnTriggerStay2D(Collider2D trig)
+    {
+        Debug.Log("staying");
+        if (trig.gameObject.name == "Cable")
         {
-            
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GameManager.movementActive = false;
+                infoDocPanel.transform.GetChild(0).gameObject.SetActive(true);
+                infoDocPanel.SetActive(true);
+                TextBubble.transform.GetChild(0).GetComponent<TextMeshPro>().text = "Faydali bir seye benziyor. Bunu saklamaliyim.";
+                trig.gameObject.SetActive(false);
+                levelCondition++;
+            }
         }
-        if (collision.gameObject.name == "Switch")
+        if (trig.gameObject.name == "Battery")
         {
-            
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GameManager.movementActive = false;
+                infoDocPanel.transform.GetChild(1).gameObject.SetActive(true);
+                infoDocPanel.SetActive(true);
+                TextBubble.gameObject.SetActive(true);
+                TextBubble.transform.GetChild(0).GetComponent<TextMeshPro>().text = "Bu da ise yarar gibi gozukuyor";
+                trig.gameObject.SetActive(false);
+                levelCondition++;
+            }
         }
+        if (trig.gameObject.name == "Lamp")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GameManager.movementActive = false;
+                infoDocPanel.transform.GetChild(2).gameObject.SetActive(true);
+                infoDocPanel.SetActive(true);
+                TextBubble.transform.GetChild(0).GetComponent<TextMeshPro>().text = "Faydali bir seye benziyor. Bunu saklamaliyim.";
+                trig.gameObject.SetActive(false);
+                levelCondition++;
+            }
+        }
+        if (trig.gameObject.name == "Switch")
+        {
+            if (Input.GetKeyDown(KeyCode.E)) 
+            {
+                GameManager.movementActive = false;
+                infoDocPanel.transform.GetChild(3).gameObject.SetActive(true);
+                infoDocPanel.SetActive(true);
+                TextBubble.gameObject.SetActive(true);
+                TextBubble.transform.GetChild(0).GetComponent<TextMeshPro>().text = "Bu da ise yarar gibi gozukuyor";
+                trig.gameObject.SetActive(false);
+                levelCondition++;
+            }
+        }
+
+        if (trig.gameObject.name == "Doc")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GameManager.movementActive = false;
+                infoDocPanel.transform.GetChild(3).gameObject.SetActive(true);
+                infoDocPanel.SetActive(true);
+                TextBubble.gameObject.SetActive(true);
+                TextBubble.transform.GetChild(0).GetComponent<TextMeshPro>().text = "Bu da ise yarar gibi gozukuyor";
+                trig.gameObject.SetActive(false);
+                levelCondition = 2;
+            }
+        }
+        if (trig.gameObject.name == "BatteryInteractive")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                trig.gameObject.transform.SetParent(this.gameObject.transform);
+
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other != null)
+        {
+            interActionGuide.SetActive(false);
+        }    
     }
 }
